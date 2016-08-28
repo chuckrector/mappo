aeea580d8dbb171b1f6fc807e2600567247155cc
Chuck <choosetheforce@gmail.com>
2016-08-27 17:25:56 -0700

    gimme dat lodash. 🙏 load moar header

--- createVerge1MapLoader.js
+++ createVerge1MapLoader.js
@@ -8,9 +8,25 @@ module.exports = (args) => {
 
   const load = () => {
     const version = reader.readByte()
+    const vsp0name = reader.readString(13)
+    const musname = reader.readString(13)
+    const layerc = reader.readByte()
+    const pmultx = reader.readByte()
+    const pdivx = reader.readByte()
+    const levelname = reader.readString(30)
+    const showname = reader.readByte()
+    const saveflag = reader.readByte()
 
     return {
-      version
+      version,
+      vsp0name,
+      musname,
+      layerc,
+      pmultx,
+      pdivx,
+      levelname,
+      showname,
+      saveflag,
     }
   }
 
--- createVerge1MapLoader.test.js
+++ createVerge1MapLoader.test.js
@@ -4,14 +4,24 @@ console.log('running tests:', __filename)
 
 const expect = require('expect')
 const createVerge1MapLoader = require('./createVerge1MapLoader.js')
+const padEnd = require('lodash/padEnd')
 
 {
   // can load header
   const loader = createVerge1MapLoader({
-    data: Buffer.from([4])
+    data: Buffer.concat([
+      Buffer.from([4]),
+      Buffer.from(padEnd('HAHN01.VSP', 13, '\0')),
+      Buffer.from(padEnd('VANGELIS.MOD', 13, '\0')),
+      Buffer.from([0, 1, 1]),
+      Buffer.from(padEnd('Village - Past', 30, '\0')),
+      Buffer.from([0, 0]),
+    ])
   })
 
   const data = loader.load()
 
   expect(data.version).toBe(4)
+  expect(data.vsp0name).toBe('HAHN01.VSP')
+  expect(data.musname).toBe('VANGELIS.MOD')
 }
