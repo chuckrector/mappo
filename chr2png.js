e6e72c18ee4d18829de3e6f39cc1e30a13756264
Chuck <choosetheforce@gmail.com>
2016-08-30 21:59:01 -0700

    chr2png cli

---
+++ chr2png.js
@@ -0,0 +1,27 @@
+const process = require('process')
+
+const fs = require('fs')
+const createVerge1PalLoader = require('./createVerge1PalLoader')
+const createVerge1ChrLoader = require('./createVerge1ChrLoader')
+const createVerge1ChrConverter = require('./createVerge1ChrConverter')
+
+const palFilename = process.argv[2]
+const chrFilename = process.argv[3]
+
+fs.readFile(palFilename, (err, diskPalData) => {
+  const palLoader = createVerge1PalLoader({data: diskPalData})
+  const palData = palLoader.load()
+
+  fs.readFile(chrFilename, (err, diskChrData) => {
+    const chrLoader = createVerge1ChrLoader({data: diskChrData})
+    const chrData = chrLoader.load()
+    const chrConverter = createVerge1ChrConverter({
+      palette: palData.pal,
+      chrs: chrData.chrs,
+    })
+
+    const png = chrConverter.convertToPng()
+
+    png.pack().pipe(fs.createWriteStream(chrFilename + '.png'))
+  })
+})
