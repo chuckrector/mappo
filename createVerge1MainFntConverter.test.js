1c5a184f215d6c428bac5ce35b6282808d35264d
Chuck <choosetheforce@gmail.com>
2016-08-31 21:45:16 -0700

    verge1 MAIN.FNT converter

---
+++ createVerge1MainFntConverter.test.js
@@ -0,0 +1,24 @@
+"use strict"
+
+const expect = require('expect')
+const createVerge1MainFntConverter = require('./createVerge1MainFntConverter')
+const fill = require('lodash/fill')
+const palette = require('./dummyPalette')
+
+{
+  // can convert MAIN.FNT font to png
+  const tileWidth = 9
+  const tileHeight = 16
+  const numtiles = 95
+  const fnt = fill(Array(tileWidth * tileHeight * numtiles))
+  const converter = createVerge1MainFntConverter({
+    palette,
+    fnt: Buffer.from(fnt)
+  })
+
+  const png = converter.convertToPng()
+
+  // 19 * 5 == 95
+  expect(png.width).toBe(tileWidth * 19)
+  expect(png.height).toBe(tileHeight * 5)
+}
