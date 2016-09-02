1ff38bc61038439b8947a066e6646bb220e20e07
Chuck <choosetheforce@gmail.com>
2016-09-02 11:55:17 -0700

    itemsdat2json cli

--- runtests-cli.sh
+++ runtests-cli.sh
@@ -12,3 +12,4 @@ node src/cli/speechspc2png.js data/VERGE.PAL data/SPEECH.SPC
 node src/cli/vsp2png.js data/HAHN01.VSP
 node src/cli/itemicondat2png.js data/VERGE.PAL data/ITEMICON.DAT
 node src/cli/miscicondat2png.js data/VERGE.PAL data/MISCICON.DAT
+node src/cli/itemsdat2json.js data/ITEMS.DAT
---
+++ src/cli/itemsdat2json.js
@@ -0,0 +1,19 @@
+const process = require('process')
+
+const fs = require('fs')
+const createVerge1ItemsDatLoader = require('../loader/createVerge1ItemsDatLoader')
+const createVerge1ItemsDatConverter = require('../converter/createVerge1ItemsDatConverter')
+
+const itemsDatFilename = process.argv[2]
+
+fs.readFile(itemsDatFilename, (err, diskItemsDatData) => {
+  const itemsDatLoader = createVerge1ItemsDatLoader({data: diskItemsDatData})
+  const itemsDatData = itemsDatLoader.load()
+  const itemsDatConverter = createVerge1ItemsDatConverter(itemsDatData)
+  const json = itemsDatConverter.convertToJson()
+  const targetFilename = itemsDatFilename + '.json'
+
+  fs.writeFileSync(targetFilename, json)
+
+  console.log('converted', itemsDatFilename, 'to', targetFilename)
+})
