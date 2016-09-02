6f7c112a480d2f445e552153423c9d41f2d1ede3
Chuck <choosetheforce@gmail.com>
2016-09-02 11:26:59 -0700

    verge1 ITEMS.DAT loader

---
+++ src/loader/createVerge1ItemsDatLoader.js
@@ -0,0 +1,59 @@
+"use strict"
+
+const createDataReader = require('../createDataReader')
+
+module.exports = (args) => {
+  const reader = createDataReader(args)
+
+  const load = () => {
+    reader.readWhitespace()
+    const numitems = reader.readStringAsByte()
+    reader.readWhitespace()
+
+    let items = []
+    for (let i = 0; i < numitems; i++) {
+      const name = reader.readStringVar()
+      reader.readWhitespace()
+      const icon = reader.readStringAsWord()
+      reader.readWhitespace()
+      const desc = reader.readStringVar()
+      reader.readWhitespace()
+      const useflag = reader.readStringAsByte()
+      reader.readWhitespace()
+      const useeffect = reader.readStringAsWord()
+      reader.readWhitespace()
+      const itemtype = reader.readStringAsByte()
+      reader.readWhitespace()
+      const equipflag = reader.readStringAsByte()
+      reader.readWhitespace()
+      const equipidx = reader.readStringAsByte()
+      reader.readWhitespace()
+      const itmprv = reader.readStringAsByte()
+      reader.readWhitespace()
+      const price = reader.readStringAsQuad()
+      reader.readWhitespace()
+
+      items.push({
+        name,
+        icon,
+        desc,
+        useflag,
+        useeffect,
+        itemtype,
+        equipflag,
+        equipidx,
+        itmprv,
+        price,
+      })
+    }
+
+    return {
+      numitems,
+      items
+    }
+  }
+
+  return {
+    load
+  }
+}
