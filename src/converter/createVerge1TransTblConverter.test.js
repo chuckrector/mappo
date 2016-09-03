1bbbfd15b325974952becb04be93385bb45d0244
Chuck <choosetheforce@gmail.com>
2016-09-03 00:10:56 -0700

    verge1 TRANS.TBL converter

---
+++ src/converter/createVerge1TransTblConverter.test.js
@@ -0,0 +1,20 @@
+"use strict"
+
+console.log('running tests:', __filename)
+
+const expect = require('expect')
+const createVerge1TransTblConverter = require('./createVerge1TransTblConverter')
+const fill = require('lodash/fill')
+
+{
+  // can convert TRANS.TBL to JSON
+  const transparencytbl = fill(Array(256 * 256), 99)
+  const converter = createVerge1TransTblConverter({
+    transparencytbl
+  })
+
+  const json = converter.convertToJson()
+  const js = JSON.parse(json)
+
+  expect(js).toEqual({transparencytbl})
+}
