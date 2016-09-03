180ff4d6a70c0ddc991d7d136548a68e77e1857c
Chuck <choosetheforce@gmail.com>
2016-09-02 22:57:51 -0700

    partydat2json cli

---
+++ src/cli/partydat2json.js
@@ -0,0 +1,19 @@
+const process = require('process')
+
+const fs = require('fs')
+const createVerge1PartyDatLoader = require('../loader/createVerge1PartyDatLoader')
+const createVerge1PartyDatConverter = require('../converter/createVerge1PartyDatConverter')
+
+const partyDatFilename = process.argv[2]
+
+fs.readFile(partyDatFilename, (err, diskPartyDatData) => {
+  const partyDatLoader = createVerge1PartyDatLoader({data: diskPartyDatData})
+  const partyDatData = partyDatLoader.load()
+  const partyDatConverter = createVerge1PartyDatConverter(partyDatData)
+  const json = partyDatConverter.convertToJson()
+  const targetFilename = partyDatFilename + '.json'
+
+  fs.writeFileSync(targetFilename, json)
+
+  console.log('converted', partyDatFilename, 'to', targetFilename)
+})
