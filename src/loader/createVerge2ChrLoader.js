e55f7fa20c3be5275fb4d70e7eb87b56fdbd3c69
Chuck <choosetheforce@gmail.com>
2016-09-04 01:25:09 -0700

    verge2 chr loader

---
+++ src/loader/createVerge2ChrLoader.js
@@ -0,0 +1,59 @@
+"use strict"
+
+const createDataReader = require('../createDataReader')
+
+module.exports = (args) => {
+  const reader = createDataReader(args)
+
+  const load = () => {
+    const version = reader.readByte()
+    const fxsize = reader.readWord()
+    const fysize = reader.readWord()
+    const hx = reader.readWord()
+    const hy = reader.readWord()
+    const hw = reader.readWord()
+    const hh = reader.readWord()
+    const totalframes = reader.readWord()
+    const imagedata = reader.readByteArrayCompressed(fxsize * fysize * totalframes)
+    const lidle = reader.readQuad()
+    const ridle = reader.readQuad()
+    const uidle = reader.readQuad()
+    const didle = reader.readQuad()
+    const lanimLength = reader.readQuad()
+    const lanim = reader.readString(lanimLength)
+    const ranimLength = reader.readQuad()
+    const ranim = reader.readString(ranimLength)
+    const uanimLength = reader.readQuad()
+    const uanim = reader.readString(uanimLength)
+    const danimLength = reader.readQuad()
+    const danim = reader.readString(danimLength)
+
+    return {
+      version,
+      fxsize,
+      fysize,
+      hx,
+      hy,
+      hw,
+      hh,
+      totalframes,
+      imagedata,
+      lidle,
+      ridle,
+      uidle,
+      didle,
+      lanimLength,
+      lanim,
+      ranimLength,
+      ranim,
+      uanimLength,
+      uanim,
+      danimLength,
+      danim,
+    }
+  }
+
+  return {
+    load
+  }
+}
---
+++ src/loader/createVerge2ChrLoader.test.js
@@ -0,0 +1,63 @@
+"use strict"
+
+console.log('running tests:', __filename)
+
+const expect = require('expect')
+const createVerge2ChrLoader = require('./createVerge2ChrLoader')
+const fill = require('lodash/fill')
+
+const version = 2
+const fxsize = 2
+const fysize = 3
+const hx = 3
+const hy = 4
+const hw = fxsize - 1
+const hh = fysize - 1
+const totalframes = 2
+const frameData = fill(Array(fxsize * fysize * 2), 99)
+
+const chrs = Buffer.concat([
+  Buffer.from([version]),
+  Buffer.from(new Uint16Array([fxsize, fysize, hx, hy, hw, hh, totalframes]).buffer),
+  // compressed image data
+  Buffer.from(new Uint32Array([3]).buffer),
+  Buffer.from([0xff, fxsize * fysize * totalframes, 99]),
+  // lidle, ridle, uidle, didle
+  Buffer.from(new Uint32Array([4, 3, 2, 1]).buffer),
+  // animations
+  Buffer.from(new Uint32Array([2]).buffer),
+  Buffer.from('F1'),
+  Buffer.from(new Uint32Array([2]).buffer),
+  Buffer.from('F2'),
+  Buffer.from(new Uint32Array([2]).buffer),
+  Buffer.from('F3'),
+  Buffer.from(new Uint32Array([2]).buffer),
+  Buffer.from('F4'),
+])
+
+{
+  // can read chrs
+  const loader = createVerge2ChrLoader({
+    data: Buffer.from(chrs)
+  })
+
+  const data = loader.load()
+
+  expect(data.version).toBe(2)
+  expect(data.fxsize).toBe(fxsize)
+  expect(data.fysize).toBe(fysize)
+  expect(data.hx).toBe(hx)
+  expect(data.hy).toBe(hy)
+  expect(data.hw).toBe(hw)
+  expect(data.hh).toBe(hh)
+  expect(data.totalframes).toBe(totalframes)
+  expect(data.imagedata.decompressed).toEqual(frameData)
+  expect(data.lidle).toBe(4)
+  expect(data.ridle).toBe(3)
+  expect(data.uidle).toBe(2)
+  expect(data.didle).toBe(1)
+  expect(data.lanim).toBe('F1')
+  expect(data.ranim).toBe('F2')
+  expect(data.uanim).toBe('F3')
+  expect(data.danim).toBe('F4')
+}
