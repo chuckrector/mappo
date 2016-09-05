4fd810aed57bcd762b0a7708336759fdb0170c44
Chuck <choosetheforce@gmail.com>
2016-09-05 01:59:30 -0700

    declarative verge2 map loader

--- src/loader/createVerge2MapLoader.js
+++ src/loader/createVerge2MapLoader.js
@@ -3,245 +3,125 @@
 const fs = require('fs')
 const createDataReader = require('../createDataReader')
 const assert = require('assert')
+const {readFormat, T} = require('../readFormat')
 
 module.exports = (args) => {
   const reader = createDataReader(args)
 
-  const loadZone = () => {
-    const name = reader.readStringFixed(40)
-    const script = reader.readWord()
-    const percent = reader.readWord()
-    const delay = reader.readWord()
-    const aaa = reader.readWord()
-    const entityscript = reader.readWord()
-
-    return {
-      name,
-      script,
-      percent,
-      delay,
-      aaa,
-      entityscript,
-    }
+  const V2_ZONE = {
+    name: T.stringFixed(40),
+    script: T.u16,
+    percent: T.u16,
+    delay: T.u16,
+    aaa: T.u16,
+    entityscript: T.u16,
   }
 
-  const loadEntity = () => {
-    const startPosition = reader.position
-
-    const x = reader.readQuad()
-    const y = reader.readQuad()
-
-    const tx = reader.readWord()
-    const ty = reader.readWord()
-
-    const facing = reader.readByte()
-    const moving = reader.readByte()
-    const movcnt = reader.readByte()
-    const frame = reader.readByte()
-    const specframe = reader.readByte()
-    const chrindex = reader.readByte()
-    const reset = reader.readByte()
-    const obsmode1 = reader.readByte()
-    const obsmode2 = reader.readByte()
-    const speed = reader.readByte()
-    const speedct = reader.readByte()
-    const delayct = reader.readByte()
-
-    const animofs = reader.readQuad()
-    const scriptofs = reader.readQuad()
-
-    const face = reader.readByte()
-    const actm = reader.readByte()
-    const movecode = reader.readByte()
-    const movescript = reader.readByte()
-    const ctr = reader.readByte()
-    const mode = reader.readByte()
-    const modePadding = reader.readByteArray(2)
-
-    const step = reader.readWord()
-    const delay = reader.readWord()
-    const stepctr = reader.readWord()
-    const delayctr = reader.readWord()
-    const data1 = reader.readWord()
-    const data2 = reader.readWord()
-    const data3 = reader.readWord()
-    const data4 = reader.readWord()
-    const data5 = reader.readWord()
-    const data6 = reader.readWord()
-
-    const actscript = reader.readQuad()
-    const expand1 = reader.readQuad()
-    const expand2 = reader.readQuad()
-    const expand3 = reader.readQuad()
-    const expand4 = reader.readQuad()
-
-    const desc = reader.readStringFixed(20)
-
-    const bytesRead = reader.position - startPosition;
-    assert.equal(bytesRead, 100, 'expected 100 bytes but read ' + bytesRead)
-
-    return {
-      x,
-      y,
-      tx,
-      ty,
-      facing,
-      moving,
-      movcnt,
-      frame,
-      specframe,
-      chrindex,
-      reset,
-      obsmode1,
-      obsmode2,
-      speed,
-      speedct,
-      delayct,
-      animofs,
-      scriptofs,
-      face,
-      actm,
-      movecode,
-      movescript,
-      ctr,
-      mode,
-      modePadding,
-      step,
-      delay,
-      stepctr,
-      delayctr,
-      data1,
-      data2,
-      data3,
-      data4,
-      data5,
-      data6,
-      actscript,
-      expand1,
-      expand2,
-      expand3,
-      expand4,
-      desc,
-    }
+  const V2_ENTITY = {
+    x: T.u32,
+    y: T.u32,
+    tx: T.u16,
+    ty: T.u16,
+    facing: T.u8,
+    moving: T.u8,
+    movcnt: T.u8,
+    frame: T.u8,
+    specframe: T.u8,
+    chrindex: T.u8,
+    reset: T.u8,
+    obsmode1: T.u8,
+    obsmode2: T.u8,
+    speed: T.u8,
+    speedct: T.u8,
+    delayct: T.u8,
+    animofs: T.u32,
+    scriptofs: T.u32,
+    face: T.u8,
+    actm: T.u8,
+    movecode: T.u8,
+    movescript: T.u8,
+    ctr: T.u8,
+    mode: T.u8,
+    modePadding: T.list(T.u8, 2),
+    step: T.u16,
+    delay: T.u16,
+    stepctr: T.u16,
+    delayctr: T.u16,
+    data1: T.u16,
+    data2: T.u16,
+    data3: T.u16,
+    data4: T.u16,
+    data5: T.u16,
+    data6: T.u16,
+    actscript: T.u32,
+    expand1: T.u32,
+    expand2: T.u32,
+    expand3: T.u32,
+    expand4: T.u32,
+    desc: T.stringFixed(20),
   }
 
-  const loadLayer = () => {
-    const pmultx = reader.readByte()
-    const pdivx = reader.readByte()
-    const pmulty = reader.readByte()
-    const pdivy = reader.readByte()
-    const sizex = reader.readWord()
-    const sizey = reader.readWord()
-    const trans = reader.readByte()
-    const hline = reader.readByte()
-    const padding = reader.readByteArray(2)
-
-    return {
-      pmultx,
-      pdivx,
-      pmulty,
-      pdivy,
-      sizex,
-      sizey,
-      trans,
-      hline,
-      padding
-    }
+  const V2_LAYER = {
+    pmultx: T.u8,
+    pdivx: T.u8,
+    pmulty: T.u8,
+    pdivy: T.u8,
+    sizex: T.u16,
+    sizey: T.u16,
+    trans: T.u8,
+    hline: T.u8,
+    padding: T.list(T.u8, 2),
   }
 
-  const load = () => {
-    const version = reader.readByteArray(6)
-    const mapEventsOffset = reader.readQuad()
-    const vspname = reader.readStringFixed(60)
-    const musname = reader.readStringFixed(60)
-    const rstring = reader.readStringFixed(20)
-    const xstart = reader.readWord()
-    const ystart = reader.readWord()
-    const wrap = reader.readByte() // TODO: verify not actually used in v2
-    const padding = reader.readByteArray(50)
-    const numlayers = reader.readByte()
+  const V2_MAP = {
+    version: T.list(T.u8, 6),
+    mapEventsOffset: T.u32,
+    vspname: T.stringFixed(60),
+    musname: T.stringFixed(60),
+    rstring: T.stringFixed(20),
+    xstart: T.u16,
+    ystart: T.u16,
+    wrap: T.u8, // TODO: verify not actually used in v2
+    padding: T.list(T.u8, 50),
 
-    let layer = []
-    for (let i = 0; i < numlayers; i++) {
-      layer.push(loadLayer())
-    }
+    numlayers: T.u8,
+    layer: T.list(V2_LAYER, ({record}) => record.numlayers),
+    layers: T.list(
+      T.compressedU16(({record, listIndex}) => {
+        return record.layer[listIndex].sizex * record.layer[listIndex].sizey
+      }),
+      ({record}) => record.numlayers
+    ),
+    obstruct: T.compressedU8(({record}) => (
+      record.layer[0].sizex * record.layer[0].sizey
+    )),
+    zone: T.compressedU8(({record}) => (
+      record.layer[0].sizex * record.layer[0].sizey
+    )),
 
-    let layers = []
-    for (let i = 0; i < numlayers; i++) {
-      const length = layer[i].sizex * layer[i].sizey
-      const decompressed = reader.readWordArrayCompressed(length)
+    numzones: T.u32,
+    zones: T.list(V2_ZONE, ({record}) => record.numzones),
+    nmchr: T.u8,
 
-      layers.push(decompressed)
-    }
+    chrlist: T.list(T.stringFixed(60), ({record}) => record.nmchr),
 
-    const baseLayerLength = layer[0].sizex * layer[0].sizey
-    const obstruct = reader.readByteArrayCompressed(baseLayerLength)
-    const zone = reader.readByteArrayCompressed(baseLayerLength)
+    entities: T.u8,
+    entity: T.list(V2_ENTITY, ({record}) => record.entities),
 
-    const numzones = reader.readQuad()
-    const zones = []
-    for (let i = 0; i < numzones; i++) {
-      zones.push(loadZone())
-    }
+    nummovescripts: T.u8,
+    msbufsize: T.u32,
+    msofstbl: T.list(T.u32, ({record}) => record.nummovescripts),
+    msbuf: T.list(T.u8, ({record}) => record.msbufsize),
 
-    const nmchr = reader.readByte()
-    const chrlist = []
-    for (let i = 0; i < nmchr; i++) {
-      chrlist.push(reader.readStringFixed(60))
-    }
+    numthings: T.u32,
 
-    const entities = reader.readByte()
-    const entity = []
-    for (let i = 0; i < entities; i++) {
-      entity.push(loadEntity())
-    }
-
-    const nummovescripts = reader.readByte()
-    const msbufsize = reader.readQuad()
-    const msofstbl = reader.readQuadArray(nummovescripts)
-    const msbuf = reader.readByteArray(msbufsize)
-
-    const numthings = reader.readQuad()
-
-    const mapevents = reader.readQuad()
-    const mapvctbl = reader.readQuadArray(mapevents)
-    const codesize = reader.readQuad()
-    const mapvc = reader.readByteArray(codesize)
-
-    return {
-      version,
-      mapEventsOffset,
-      vspname,
-      musname,
-      rstring,
-      xstart,
-      ystart,
-      wrap,
-      padding,
-      numlayers,
-      layer,
-      layers,
-      obstruct,
-      zone,
-      numzones,
-      zones,
-      nmchr,
-      chrlist,
-      entities,
-      entity,
-      nummovescripts,
-      msbufsize,
-      msofstbl,
-      msbuf,
-      numthings,
-      mapevents,
-      mapvctbl,
-      codesize,
-      mapvc,
-    }
+    mapevents: T.u32,
+    mapvctbl: T.list(T.u32, ({record}) => record.mapevents),
+    codesize: T.u32,
+    mapvc: T.list(T.u8, ({record}) => record.codesize),
   }
 
   return {
-    load
+    load: () => readFormat({format: V2_MAP, reader})
   }
 }
