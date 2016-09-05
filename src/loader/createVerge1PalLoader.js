80afe6438ab19d8205bd0c15abdb3d7233a79592
Chuck <choosetheforce@gmail.com>
2016-09-04 23:50:20 -0700

    declarative verge1 pal loader

--- src/loader/createVerge1PalLoader.js
+++ src/loader/createVerge1PalLoader.js
@@ -1,19 +1,15 @@
 "use strict"
 
 const createDataReader = require('../createDataReader')
+const {readFormat, T} = require('../readFormat')
 
 module.exports = (args) => {
   const reader = createDataReader(args)
 
-  const load = () => {
-    const pal = reader.readByteArray(256 * 3)
-
-    return {
-      pal
-    }
-  }
-
   return {
-    load
+    load: () => readFormat({
+      format: {pal: T.list(T.u8, 256 * 3)},
+      reader
+    })
   }
 }
