a0adfd8169d13e34d73e348fddaaa6ef7a6f9597
Chuck <choosetheforce@gmail.com>
2016-09-04 23:49:15 -0700

    declarative verge1 cr2 loader

--- src/loader/createVerge1Cr2Loader.js
+++ src/loader/createVerge1Cr2Loader.js
@@ -1,22 +1,15 @@
 "use strict"
 
 const createDataReader = require('../createDataReader')
+const {readFormat, T} = require('../readFormat')
 
 module.exports = (args) => {
   const reader = createDataReader(args)
 
-  const load = () => {
-    const tileWidth = 96
-    const tileHeight = 96
-
-    const chr2 = reader.readByteArray(tileWidth * tileHeight)
-
-    return {
-      chr2
-    }
-  }
-
   return {
-    load
+    load: () => readFormat({
+      format: {chr2: T.list(T.u8, 96 * 96)},
+      reader
+    })
   }
 }
