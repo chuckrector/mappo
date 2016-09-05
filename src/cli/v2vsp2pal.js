18dfd8c22baad97f2b682a2ea839ad5a5651cf5e
Chuck <choosetheforce@gmail.com>
2016-09-05 04:13:45 -0700

    v2vsp2pal should output VERGE2.PAL alongside its source

--- src/cli/v2vsp2pal.js
+++ src/cli/v2vsp2pal.js
@@ -1,6 +1,7 @@
 const process = require('process')
 
 const fs = require('fs')
+const path = require('path')
 const createDataReader = require('../createDataReader')
 
 const vspFilename = process.argv[2]
@@ -9,7 +10,10 @@ const vspData = fs.readFileSync(vspFilename)
 const reader = createDataReader({data: vspData})
 reader.readWord()
 const palette = reader.readByteArray(256 * 3)
-const targetFilename = 'VERGE2.PAL'
+const targetFilename = path.format({
+  dir: path.dirname(vspFilename),
+  base: 'VERGE2.PAL',
+})
 fs.writeFileSync(targetFilename, Buffer.from(palette))
 
 console.log('converted', vspFilename, 'to', targetFilename)
