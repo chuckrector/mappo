8c0b9129328dacc8f3b79831e8cae7b1e32bbba6
Chuck <choosetheforce@gmail.com>
2016-09-05 02:39:38 -0700

    declarative verge1 MISCICON.DAT loader

--- src/loader/createVerge1MiscIconDatLoader.js
+++ src/loader/createVerge1MiscIconDatLoader.js
@@ -1,32 +1,19 @@
 "use strict"
 
 const createDataReader = require('../createDataReader')
+const {readFormat, T} = require('../readFormat')
 
 module.exports = (args) => {
   const reader = createDataReader(args)
 
-  const load = () => {
-    const menuptrTileWidth = 16
-    const menuptrTileHeight = 16
-    const itmptrTileWidth = 24
-    const itmptrTileHeight = 24
-    const charptrTileWidth = 24
-    const charptrTileHeight = 40
-
-    const numtiles = reader.readByte()
-    const menuptr = reader.readByteArray(menuptrTileWidth * menuptrTileHeight)
-    const itmptr = reader.readByteArray(itmptrTileWidth * itmptrTileHeight)
-    const charptr = reader.readByteArray(charptrTileWidth * charptrTileHeight)
-
-    return {
-      numtiles,
-      menuptr,
-      itmptr,
-      charptr,
-    }
+  const V1_MISCICON_DAT = {
+    numtiles: T.u8,
+    menuptr: T.list(T.u8, 16 * 16),
+    itmptr: T.list(T.u8, 24 * 24),
+    charptr: T.list(T.u8, 24 * 40),
   }
 
   return {
-    load
+    load: () => readFormat({format: V1_MISCICON_DAT, reader})
   }
 }
