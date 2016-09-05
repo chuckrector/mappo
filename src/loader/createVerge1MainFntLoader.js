6e7ce12ddf5bfaad127823ef996a5c800d9922a4
Chuck <choosetheforce@gmail.com>
2016-09-05 02:48:47 -0700

    declarative verge1 MAIN.FNT loader

--- src/loader/createVerge1MainFntLoader.js
+++ src/loader/createVerge1MainFntLoader.js
@@ -1,23 +1,15 @@
 "use strict"
 
 const createDataReader = require('../createDataReader')
+const {readFormat, T} = require('../readFormat')
 
 module.exports = (args) => {
   const reader = createDataReader(args)
 
-  const load = () => {
-    const tileWidth = 9
-    const tileHeight = 16
-    const numtiles = 95
-
-    const fnt2 = reader.readByteArray(tileWidth * tileHeight * numtiles)
-
-    return {
-      fnt2
-    }
-  }
-
   return {
-    load
+    load: () => readFormat({
+      format: {fnt2: T.list(T.u8, 9 * 16 * 95)},
+      reader
+    })
   }
 }
