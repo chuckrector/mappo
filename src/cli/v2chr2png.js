99962718804276b6f27d698e746a188a7ce7e08f
Chuck <choosetheforce@gmail.com>
2016-09-05 23:59:25 -0700

    use sync file readers in cli

--- src/cli/v2chr2png.js
+++ src/cli/v2chr2png.js
@@ -8,26 +8,24 @@ const createVerge2ChrConverter = require('../converter/createVerge2ChrConverter'
 const palFilename = process.argv[2]
 const chrFilename = process.argv[3]
 
-fs.readFile(palFilename, (err, diskPalData) => {
-  const palLoader = createVerge1PalLoader({data: diskPalData})
-  const palData = palLoader.load()
+const diskPalData = fs.readFileSync(palFilename)
+const palLoader = createVerge1PalLoader({data: diskPalData})
+const palData = palLoader.load()
 
-  fs.readFile(chrFilename, (err, diskChrData) => {
-    const chrLoader = createVerge2ChrLoader({data: diskChrData})
-    const chrData = chrLoader.load()
-    const chrConverter = createVerge2ChrConverter({
-      palette: palData.pal,
-      fxsize: chrData.fxsize,
-      fysize: chrData.fysize,
-      totalframes: chrData.totalframes,
-      imagedata: chrData.imagedata.decompressed,
-    })
-
-    const png = chrConverter.convertToPng()
-    const targetFilename = chrFilename + '.png'
-
-    png.pack().pipe(fs.createWriteStream(targetFilename))
-
-    console.log('converted', chrFilename, 'to', targetFilename)
-  })
+const diskChrData = fs.readFileSync(chrFilename)
+const chrLoader = createVerge2ChrLoader({data: diskChrData})
+const chrData = chrLoader.load()
+const chrConverter = createVerge2ChrConverter({
+  palette: palData.pal,
+  fxsize: chrData.fxsize,
+  fysize: chrData.fysize,
+  totalframes: chrData.totalframes,
+  imagedata: chrData.imagedata.decompressed,
 })
+
+const png = chrConverter.convertToPng()
+const targetFilename = chrFilename + '.png'
+
+png.pack().pipe(fs.createWriteStream(targetFilename))
+
+console.log('converted', chrFilename, 'to', targetFilename)
