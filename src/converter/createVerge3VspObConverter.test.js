62e7f1b7c39f9a94eab147a69700a736465a6906
Chuck <choosetheforce@gmail.com>
2016-09-05 21:25:39 -0700

    verge3 vsp obstructions converter

---
+++ src/converter/createVerge3VspObConverter.test.js
@@ -0,0 +1,20 @@
+"use strict"
+
+const expect = require('expect')
+const createVerge3VspObConverter = require('./createVerge3VspObConverter')
+const fill = require('lodash/fill')
+const colorDepth = require('./colorDepth')
+const palette = require('../dummyPalette')
+
+{
+  // can convert vsp obstruction tile data to png
+  const converter = createVerge3VspObConverter({
+    numtiles: 21,
+    obs: fill(Array(16 * 16 * 3 * 12)),
+  })
+
+  const png = converter.convertToPng()
+
+  expect(png.width).toBe(16 * 20)
+  expect(png.height).toBe(16 * 2)
+}
