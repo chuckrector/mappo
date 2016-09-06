553089938d48d36a0ec83ce80c23343c30fc659a
Chuck <choosetheforce@gmail.com>
2016-09-05 23:54:11 -0700

    use sync file readers in cli

--- src/cli/itemicondat2png.js
+++ src/cli/itemicondat2png.js
@@ -8,24 +8,22 @@ const createVerge1ItemIconDatConverter = require('../converter/createVerge1ItemI
 const palFilename = process.argv[2]
 const itemIconDatFilename = process.argv[3]
 
-fs.readFile(palFilename, (err, diskPalData) => {
-  const palLoader = createVerge1PalLoader({data: diskPalData})
-  const palData = palLoader.load()
+const diskPalData = fs.readFileSync(palFilename)
+const palLoader = createVerge1PalLoader({data: diskPalData})
+const palData = palLoader.load()
 
-  fs.readFile(itemIconDatFilename, (err, diskItemIconDatData) => {
-    const itemIconDatLoader = createVerge1ItemIconDatLoader({data: diskItemIconDatData})
-    const itemIconDatData = itemIconDatLoader.load()
-    const itemIconDatConverter = createVerge1ItemIconDatConverter({
-      palette: palData.pal,
-      numtiles: itemIconDatData.numtiles,
-      itemicons: itemIconDatData.itemicons,
-    })
-
-    const png = itemIconDatConverter.convertToPng()
-    const targetFilename = itemIconDatFilename + '.png'
-
-    png.pack().pipe(fs.createWriteStream(targetFilename))
-
-    console.log('converted', itemIconDatFilename, 'to', targetFilename)
-  })
+const diskItemIconDatData = fs.readFileSync(itemIconDatFilename)
+const itemIconDatLoader = createVerge1ItemIconDatLoader({data: diskItemIconDatData})
+const itemIconDatData = itemIconDatLoader.load()
+const itemIconDatConverter = createVerge1ItemIconDatConverter({
+  palette: palData.pal,
+  numtiles: itemIconDatData.numtiles,
+  itemicons: itemIconDatData.itemicons,
 })
+
+const png = itemIconDatConverter.convertToPng()
+const targetFilename = itemIconDatFilename + '.png'
+
+png.pack().pipe(fs.createWriteStream(targetFilename))
+
+console.log('converted', itemIconDatFilename, 'to', targetFilename)
