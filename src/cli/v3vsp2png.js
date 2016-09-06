bb8f710b52c23cc5f42d79abda978ea5306d1bc3
Chuck <choosetheforce@gmail.com>
2016-09-06 00:00:28 -0700

    use sync file readers in cli

--- src/cli/v3vsp2png.js
+++ src/cli/v3vsp2png.js
@@ -5,17 +5,16 @@ const createVerge3VspLoader = require('../loader/createVerge3VspLoader')
 const createVerge3VspConverter = require('../converter/createVerge3VspConverter')
 
 const vspFilename = process.argv[2]
-fs.readFile(vspFilename, (err, data) => {
-  const loader = createVerge3VspLoader({data})
-  const vsp = loader.load()
-  const converter = createVerge3VspConverter({
-    numtiles: vsp.numtiles,
-    tiledatabuf: vsp.tiledatabuf.decompressed,
-  })
-  const png = converter.convertToPng()
-  const targetFilename = vspFilename + '.png'
-
-  png.pack().pipe(fs.createWriteStream(targetFilename))
-
-  console.log('converted', vspFilename, 'to', targetFilename)
+const data = fs.readFileSync(vspFilename)
+const loader = createVerge3VspLoader({data})
+const vsp = loader.load()
+const converter = createVerge3VspConverter({
+  numtiles: vsp.numtiles,
+  tiledatabuf: vsp.tiledatabuf.decompressed,
 })
+const png = converter.convertToPng()
+const targetFilename = vspFilename + '.png'
+
+png.pack().pipe(fs.createWriteStream(targetFilename))
+
+console.log('converted', vspFilename, 'to', targetFilename)
