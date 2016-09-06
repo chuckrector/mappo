fa14ddef5870d0efad8f55659b4367a3104cb88e
Chuck <choosetheforce@gmail.com>
2016-09-05 23:51:39 -0700

    use sync file readers in cli

--- src/cli/chr2png.js
+++ src/cli/chr2png.js
@@ -8,23 +8,21 @@ const createVerge1ChrConverter = require('../converter/createVerge1ChrConverter'
 const palFilename = process.argv[2]
 const chrFilename = process.argv[3]
 
-fs.readFile(palFilename, (err, diskPalData) => {
-  const palLoader = createVerge1PalLoader({data: diskPalData})
-  const palData = palLoader.load()
+const diskPalData = fs.readFileSync(palFilename)
+const palLoader = createVerge1PalLoader({data: diskPalData})
+const palData = palLoader.load()
 
-  fs.readFile(chrFilename, (err, diskChrData) => {
-    const chrLoader = createVerge1ChrLoader({data: diskChrData})
-    const chrData = chrLoader.load()
-    const chrConverter = createVerge1ChrConverter({
-      palette: palData.pal,
-      chrs: chrData.chrs,
-    })
-
-    const png = chrConverter.convertToPng()
-    const targetFilename = chrFilename + '.png'
-
-    png.pack().pipe(fs.createWriteStream(targetFilename))
-
-    console.log('converted', chrFilename, 'to', targetFilename)
-  })
+const diskChrData = fs.readFileSync(chrFilename)
+const chrLoader = createVerge1ChrLoader({data: diskChrData})
+const chrData = chrLoader.load()
+const chrConverter = createVerge1ChrConverter({
+  palette: palData.pal,
+  chrs: chrData.chrs,
 })
+
+const png = chrConverter.convertToPng()
+const targetFilename = chrFilename + '.png'
+
+png.pack().pipe(fs.createWriteStream(targetFilename))
+
+console.log('converted', chrFilename, 'to', targetFilename)
