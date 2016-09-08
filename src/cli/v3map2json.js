9a3a48231d430f4673dd42b1a7c08ea26af964e1
Chuck <choosetheforce@gmail.com>
2016-09-08 14:51:35 -0700

    simplify

--- src/cli/v3map2json.js
+++ src/cli/v3map2json.js
@@ -2,17 +2,14 @@ const process = require('process')
 
 const fs = require('fs')
 const createVerge3MapLoader = require('../loader/createVerge3MapLoader')
-const createVerge3MapConverter = require('../converter/createVerge3MapConverter')
 
 const mapFilename = process.argv[2]
 
 const diskMapData = fs.readFileSync(mapFilename)
 const mapLoader = createVerge3MapLoader({data: diskMapData})
 const mapData = mapLoader.load()
-const mapConverter = createVerge3MapConverter(mapData)
-const json = mapConverter.convertToJson()
 const targetFilename = mapFilename + '.json'
 
-fs.writeFileSync(targetFilename, json)
+fs.writeFileSync(targetFilename, JSON.stringify(mapData))
 
 console.log('converted', mapFilename, 'to', targetFilename)
