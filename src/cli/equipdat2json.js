ca073a8c73593e865b35826c83e61c8123deb62e
Chuck <choosetheforce@gmail.com>
2016-09-08 13:57:06 -0700

    simplify

--- src/cli/equipdat2json.js
+++ src/cli/equipdat2json.js
@@ -2,17 +2,14 @@ const process = require('process')
 
 const fs = require('fs')
 const createVerge1EquipDatLoader = require('../loader/createVerge1EquipDatLoader')
-const createVerge1EquipDatConverter = require('../converter/createVerge1EquipDatConverter')
 
 const equipDatFilename = process.argv[2]
 
 const diskEquipDatData = fs.readFileSync(equipDatFilename)
 const equipDatLoader = createVerge1EquipDatLoader({data: diskEquipDatData})
 const equipDatData = equipDatLoader.load()
-const equipDatConverter = createVerge1EquipDatConverter(equipDatData)
-const json = equipDatConverter.convertToJson()
 const targetFilename = equipDatFilename + '.json'
 
-fs.writeFileSync(targetFilename, json)
+fs.writeFileSync(targetFilename, JSON.stringify(equipDatData))
 
 console.log('converted', equipDatFilename, 'to', targetFilename)
