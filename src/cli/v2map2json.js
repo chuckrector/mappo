ff38e09142a063d65ef83023108d73b41bb9f94b
Chuck <choosetheforce@gmail.com>
2016-09-08 14:50:52 -0700

    simplify

--- src/cli/v2map2json.js
+++ src/cli/v2map2json.js
@@ -2,17 +2,14 @@ const process = require('process')
 
 const fs = require('fs')
 const createVerge2MapLoader = require('../loader/createVerge2MapLoader')
-const createVerge2MapConverter = require('../converter/createVerge2MapConverter')
 
 const mapFilename = process.argv[2]
 
 const diskMapData = fs.readFileSync(mapFilename)
 const mapLoader = createVerge2MapLoader({data: diskMapData})
 const mapData = mapLoader.load()
-const mapConverter = createVerge2MapConverter(mapData)
-const json = mapConverter.convertToJson()
 const targetFilename = mapFilename + '.json'
 
-fs.writeFileSync(targetFilename, json)
+fs.writeFileSync(targetFilename, JSON.stringify(mapData))
 
 console.log('converted', mapFilename, 'to', targetFilename)
--- src/converter/createVerge2MapConverter.js
+++
@@ -1,7 +0,0 @@
-"use strict"
-
-module.exports = (data) => {
-  return {
-    convertToJson: () => JSON.stringify(data)
-  }
-}
