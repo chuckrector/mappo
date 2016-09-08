e94e80876cc0b88808a99ed1ac8f8e11df2a77cb
Chuck <choosetheforce@gmail.com>
2016-09-08 14:49:23 -0700

    simplify

--- src/cli/transtbl2json.js
+++ src/cli/transtbl2json.js
@@ -2,17 +2,14 @@ const process = require('process')
 
 const fs = require('fs')
 const createVerge1TransTblLoader = require('../loader/createVerge1TransTblLoader')
-const createVerge1TransTblConverter = require('../converter/createVerge1TransTblConverter')
 
 const transTblFilename = process.argv[2]
 
 const diskTransTblData = fs.readFileSync(transTblFilename)
 const transTblLoader = createVerge1TransTblLoader({data: diskTransTblData})
 const transTblData = transTblLoader.load()
-const transTblConverter = createVerge1TransTblConverter(transTblData)
-const json = transTblConverter.convertToJson()
 const targetFilename = transTblFilename + '.json'
 
-fs.writeFileSync(targetFilename, json)
+fs.writeFileSync(targetFilename, JSON.stringify(transTblData))
 
 console.log('converted', transTblFilename, 'to', targetFilename)
