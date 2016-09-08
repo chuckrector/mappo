595bfb977b52ac7c2c71ef451fb507bcb8fa82f9
Chuck <choosetheforce@gmail.com>
2016-09-08 13:47:51 -0700

    formatting

--- src/readFormat.js
+++ src/readFormat.js
@@ -2,65 +2,49 @@
 
 const zlib = require('zlib')
 
-const lengthCalculator = (length, {reader, record, listIndex}) => {
-  if (typeof length === 'function') {
-    return length({reader, record, listIndex})
-  }
+const T = {}
 
-  return length
-}
-
-const T = {
-  u8: ({reader}) => reader.readByte(),
-  u16: ({reader}) => reader.readWord(),
-  u32: ({reader}) => reader.readQuad(),
-  f64: ({reader}) => reader.readDouble(),
-
-  whitespace: ({reader}) => reader.readWhitespace(),
-  string: ({reader}) => reader.readString(),
-  stringU8: ({reader}) => reader.readStringAsByte(),
-  stringU16: ({reader}) => reader.readStringAsWord(),
-  stringU32: ({reader}) => reader.readStringAsQuad(),
-  stringFixed: (length) => {
-    return ({reader, record, listIndex}) => {
-      return reader.readStringFixed(lengthCalculator(length, {reader, record, listIndex}))
-    }
-  },
-  stringNullTerminated: ({reader}) => reader.readStringNullTerminated(),
-
-  compressedU8: (length) => {
-    return ({reader, record, listIndex}) => {
-      return reader.readByteArrayCompressed(lengthCalculator(length, {reader, record, listIndex}))
-    }
-  },
-  compressedU16: (length) => {
-    return ({reader, record, listIndex}) => {
-      return reader.readWordArrayCompressed(lengthCalculator(length, {reader, record, listIndex}))
-    }
-  },
-
-  zlib: (length) => {
-    return ({reader, record, listIndex}) => {
-      return reader.readZlib(lengthCalculator(length, {reader, record, listIndex}))
-    }
-  },
-
-  palette6bit: ({reader}) => {
-    const _6bit = reader.readByteArray(256 * 3)
-    const _8bit = _6bit.map(v => v * 4)
-
-    return {
-      _6bit,
-      _8bit,
-    }
+T.u8 = ({reader}) => reader.readByte()
+T.u16 = ({reader}) => reader.readWord()
+T.u32 = ({reader}) => reader.readQuad()
+T.f64 = ({reader}) => reader.readDouble()
+T.whitespace = ({reader}) => reader.readWhitespace()
+T.string = ({reader}) => reader.readString()
+T.stringU8 = ({reader}) => reader.readStringAsByte()
+T.stringU16 = ({reader}) => reader.readStringAsWord()
+T.stringU32 = ({reader}) => reader.readStringAsQuad()
+T.stringNullTerminated = ({reader}) => reader.readStringNullTerminated()
+T.stringFixed = (length) => {
+  return ({reader, record, listIndex}) => {
+    return reader.readStringFixed(lengthCalculator(length, {reader, record, listIndex}))
   }
 }
 
-const resolve = (formatOrFunction, {reader, record, listIndex}) => {
-  if (typeof formatOrFunction === 'function') {
-    return formatOrFunction({reader, record, listIndex})
-  } else {
-    return readFormat({format: formatOrFunction, reader, listIndex})
+T.compressedU8 = (length) => {
+  return ({reader, record, listIndex}) => {
+    return reader.readByteArrayCompressed(lengthCalculator(length, {reader, record, listIndex}))
+  }
+}
+
+T.compressedU16 = (length) => {
+  return ({reader, record, listIndex}) => {
+    return reader.readWordArrayCompressed(lengthCalculator(length, {reader, record, listIndex}))
+  }
+}
+
+T.zlib = (length) => {
+  return ({reader, record, listIndex}) => {
+    return reader.readZlib(lengthCalculator(length, {reader, record, listIndex}))
+  }
+}
+
+T.palette6bit = ({reader}) => {
+  const _6bit = reader.readByteArray(256 * 3)
+  const _8bit = _6bit.map(v => v * 4)
+
+  return {
+    _6bit,
+    _8bit,
   }
 }
 
@@ -78,6 +62,22 @@ T.list = (formatOrFunction, length) => {
     }
 
     return recordList
+  }
+}
+
+const lengthCalculator = (length, {reader, record, listIndex}) => {
+  if (typeof length === 'function') {
+    return length({reader, record, listIndex})
+  }
+
+  return length
+}
+
+const resolve = (formatOrFunction, {reader, record, listIndex}) => {
+  if (typeof formatOrFunction === 'function') {
+    return formatOrFunction({reader, record, listIndex})
+  } else {
+    return readFormat({format: formatOrFunction, reader, listIndex})
   }
 }
 
