edc12358cb627413ac1ab06af35430a3c950c3a3
Chuck <choosetheforce@gmail.com>
2016-09-08 14:46:00 -0700

    simplify

--- src/cli/itemsdat2json.js
+++ src/cli/itemsdat2json.js
@@ -2,17 +2,14 @@ const process = require('process')
 
 const fs = require('fs')
 const createVerge1ItemsDatLoader = require('../loader/createVerge1ItemsDatLoader')
-const createVerge1ItemsDatConverter = require('../converter/createVerge1ItemsDatConverter')
 
 const itemsDatFilename = process.argv[2]
 
 const diskItemsDatData = fs.readFileSync(itemsDatFilename)
 const itemsDatLoader = createVerge1ItemsDatLoader({data: diskItemsDatData})
 const itemsDatData = itemsDatLoader.load()
-const itemsDatConverter = createVerge1ItemsDatConverter(itemsDatData)
-const json = itemsDatConverter.convertToJson()
 const targetFilename = itemsDatFilename + '.json'
 
-fs.writeFileSync(targetFilename, json)
+fs.writeFileSync(targetFilename, JSON.stringify(itemsDatData))
 
 console.log('converted', itemsDatFilename, 'to', targetFilename)
