739e332933e6963c38dba62066659fb84fd90819
Chuck <choosetheforce@gmail.com>
2016-09-08 17:33:58 -0700

    simplify

--- src/loader/createVerge1ItemIconDatLoader.test.js
+++ src/loader/createVerge1ItemIconDatLoader.test.js
@@ -3,6 +3,7 @@
 const expect = require('expect')
 const createVerge1ItemIconDatLoader = require('./createVerge1ItemIconDatLoader')
 const fill = require('lodash/fill')
+const {makeBuffer, B} = require('../makeBuffer')
 
 const tileWidth = 16
 const tileHeight = 16
@@ -12,9 +13,9 @@ const itemicons = fill(Array(tileWidth * tileHeight * numtiles), 99)
 {
   // can read ITEMICON.DAT
   const loader = createVerge1ItemIconDatLoader({
-    data: Buffer.concat([
-      Buffer.from([21]),
-      Buffer.from(itemicons)
+    data: makeBuffer([
+      B.u8(21),
+      B.list(B.u8, itemicons)
     ])
   })
 
