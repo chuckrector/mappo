b405c116710351a4a131a0f973798619244d9666
Chuck <choosetheforce@gmail.com>
2016-09-08 23:22:05 -0700

    simplify

--- src/dummyVerge1Map.js
+++ src/dummyVerge1Map.js
@@ -10,23 +10,23 @@ const mapHeader = makeBuffer([
   B.u8(4),
   B.stringFixed(13, 'HAHN01.VSP'),
   B.stringFixed(13, 'VANGELIS.MOD'),
-  B.list(B.u8, [0, 1, 1]),
+  B.u8([0, 1, 1]),
   B.stringFixed(30, 'Village - Past'),
-  B.list(B.u8, [1, 0]),
-  B.list(B.u16, [21, 1]),
-  B.list(B.u8, [1, 1]),
-  B.list(B.u16, [mapWidth, mapHeight]),
+  B.u8([1, 0]),
+  B.u16([21, 1]),
+  B.u8([1, 1]),
+  B.u16([mapWidth, mapHeight]),
   B.u8(0),
-  B.list(B.u8, fill(Array(27), 0))
+  B.u8(fill(Array(27), 0))
 ])
 
 const mapBackgroundLayerData = fill(Array(mapWidth * mapHeight), 33)
 const mapForegroundLayerData = fill(Array(mapWidth * mapHeight), 44)
 const mapObstructionLayerData = fill(Array(mapWidth * mapHeight), 55)
 const mapLayers = makeBuffer([
-  B.list(B.u16, mapBackgroundLayerData),
-  B.list(B.u16, mapForegroundLayerData),
-  B.list(B.u8, mapObstructionLayerData),
+  B.u16(mapBackgroundLayerData),
+  B.u16(mapForegroundLayerData),
+  B.u8(mapObstructionLayerData),
 ])
 
 const zone = makeBuffer([
@@ -37,7 +37,7 @@ const zone = makeBuffer([
   // callevent
   B.u16(1),
   // percent, delay, aaa
-  B.list(B.u8, [255, 0, 1]),
+  B.u8([255, 0, 1]),
   // savedesc
   B.stringFixed(30, 'Rodne'),
   // padding
@@ -54,23 +54,23 @@ const chrList = makeBuffer(
 
 const entity = makeBuffer([
   // x, y
-  B.list(B.u16, [2, 1]),
+  B.u16([2, 1]),
   // facing, moving, movcnt, framectr, specframe, chrindex, movecode, activmode, obsmode
-  B.list(B.u8, [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]),
+  B.u8([9, 8, 7, 6, 5, 4, 3, 2, 1, 0, 0, 0]),
   // actscript, movescript
-  B.list(B.u32, [2, 1]),
+  B.u32([2, 1]),
   // speed, speedct
-  B.list(B.u8, [2, 1]),
+  B.u8([2, 1]),
   // step..y2
-  B.list(B.u16, [12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]),
+  B.u16([12, 11, 10, 9, 8, 7, 6, 5, 4, 3, 2, 1]),
   // curcmd, cmdarg
-  B.list(B.u8, [2, 1]),
+  B.u8([2, 1]),
   // scriptofs
   B.u32(1),
   // face, chasing, chasespeed, chasedist
-  B.list(B.u8, [4, 3, 2, 1]),
+  B.u8([4, 3, 2, 1]),
   // cx, cy
-  B.list(B.u16, [2, 1]),
+  B.u16([2, 1]),
   // expand
   B.u32(1),
   // entitydesc
@@ -88,16 +88,16 @@ const scripts = makeBuffer([
   // msbufsize
   B.u32(50),
   // msofstbl
-  B.list(B.u32, [0, 10, 20, 30, 40]),
+  B.u32([0, 10, 20, 30, 40]),
   // msbuf
-  B.list(B.u8, fill(Array(50), 99)),
+  B.u8(fill(Array(50), 99)),
   // numscripts, scriptofstb,
-  B.list(B.u32, [3, 0, 10, 20]),
+  B.u32([3, 0, 10, 20]),
   // mapvc
-  B.list(B.u8, fill(Array(30), 88)),
+  B.u8(fill(Array(30), 88)),
 ])
 
-const map = Buffer.concat([
+const map = makeBuffer([
   mapHeader,
   mapLayers,
   mapZones,
