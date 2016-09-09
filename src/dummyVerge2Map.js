5de7faefe6a3d4abe6b381189a03e7e4603fbb09
Chuck <choosetheforce@gmail.com>
2016-09-08 18:53:24 -0700

    simplify

--- src/dummyVerge2Map.js
+++ src/dummyVerge2Map.js
@@ -1,140 +1,137 @@
 "use strict"
 
-const padEnd = require('lodash/padEnd')
 const fill = require('lodash/fill')
+const {makeBuffer, B} = require('./makeBuffer')
 
 const mapWidth = 2
 const mapHeight = 3
-const mapHeader = Buffer.concat([
+const mapHeader = makeBuffer([
   // version
-  Buffer.from([77, 65, 80, 249, 53, 0]),
+  B.u8([77, 65, 80, 249, 53, 0]),
   // mapEventsOffset
-  Buffer.from(new Uint32Array([12345]).buffer),
+  B.u32(12345),
   // vspname
-  Buffer.from(padEnd('grue0040.vsp', 60, '\0')),
+  B.stringFixed(60, 'grue0040.vsp'),
   // musname
-  Buffer.from(padEnd('VANGELIS.MOD', 60, '\0')),
+  B.stringFixed(60, 'VANGELIS.MOD'),
   // rstring
-  Buffer.from(padEnd('12E', 20, '\0')),
+  B.stringFixed(20, '12E'),
   // xstart, ystart
-  Buffer.from(new Uint16Array([1, 2]).buffer),
+  B.u16([1 ,2]),
   // wrap
-  Buffer.from([1]),
+  B.u8(1),
   // padding
-  Buffer.from(fill(Array(50), 99)),
+  B.u8(fill(Array(50), 99)),
 ])
 
-const mapLayers = Buffer.concat([
+const rawLayer = [1, ...fill(Array((2 * 3) - 2), 2), 3]
+const mapLayers = makeBuffer([
   // numlayers
-  Buffer.from([2]),
+  B.u8(2),
 
   // -- first layer info --
 
   // pmultx, pdivx, pmulty, pdivy
-  Buffer.from([1, 0, 1, 0]),
+  B.u8([1, 0, 1, 0]),
   // sizex, sizey
-  Buffer.from(new Uint16Array([2, 3]).buffer),
+  B.u16([2, 3]),
   // trans, hline, padding (2 bytes)
-  Buffer.from([4, 3, 2, 1]),
+  B.u8([4, 3, 2, 1]),
 
   // -- second layer info --
 
   // pmultix, pdivx, pmulty, pdivy
-  Buffer.from([1, 0, 1, 0]),
+  B.u8([1, 0, 1, 0]),
   // sizex, sizey
-  Buffer.from(new Uint16Array([2, 3]).buffer),
+  B.u16([2, 3]),
   // trans, hline, padding (2 bytes)
-  Buffer.from([4, 3, 2, 1]),
+  B.u8([4, 3, 2, 1]),
 
   // -- first layer compressed data --
 
-  Buffer.from(new Uint32Array([4 * 2]).buffer),
-  Buffer.from(new Uint16Array([1, ((2 * 3) - 2) | 0xff00, 2, 3]).buffer),
+  B.compressedU16(rawLayer),
 
   // -- second layer compressed data --
 
-  Buffer.from(new Uint32Array([4 * 2]).buffer),
-  Buffer.from(new Uint16Array([1, ((2 * 3) - 2) | 0xff00, 2, 3]).buffer),
+  B.compressedU16(rawLayer),
 
   // -- obstruction layer compressed data --
 
-  Buffer.from(new Uint32Array([5]).buffer),
-  Buffer.from([1, 0xff, ((2 * 3) - 2), 2, 3]),
+  B.compressedU8(rawLayer),
 
   // -- zone layer compressed data --
 
-  Buffer.from(new Uint32Array([5]).buffer),
-  Buffer.from([1, 0xff, ((2 * 3) - 2), 2, 3]),
+  B.compressedU8(rawLayer),
 ])
 
-const zone = Buffer.concat([
+const zone = makeBuffer([
   // zonename
-  Buffer.from(padEnd('Default', 40, '\0')),
+  B.stringFixed(40, 'Default'),
   // script, percent, delay, aaa, entityscript
-  Buffer.from(new Uint16Array([5, 4, 3, 2, 1]).buffer),
+  B.u16([5, 4, 3, 2, 1]),
 ])
 
-const mapZones = Buffer.concat([
+const mapZones = makeBuffer([
   // numzones
-  Buffer.from(new Uint32Array([2]).buffer),
-  Buffer.concat(fill(Array(2), zone)),
+  B.u32(2),
+  makeBuffer(fill(Array(2), zone)),
 ])
 
-const chrList = Buffer.concat([
+const chrList = makeBuffer([
   // nmchr
-  Buffer.from([3]),
-  Buffer.concat(fill(Array(3), Buffer.from(padEnd('DARIN.CHR', 60, '\0')))),
+  B.u8(3),
+  makeBuffer(fill(Array(3), B.stringFixed(60, 'DARIN.CHR'))),
 ])
 
-const entity = Buffer.concat([
+const entity = makeBuffer([
   // x, y
-  Buffer.from(new Uint32Array([2, 1]).buffer),
+  B.u32([2, 1]),
   // tx, ty
-  Buffer.from(new Uint16Array([4, 3]).buffer),
+  B.u16([4, 3]),
   // facing, moving, movcnt, frame, specframe, chrindex, reset, obsmode1, obsmode2, speed, speedct, delayct
-  Buffer.from(fill(Array(12), 99)),
+  B.u8(fill(Array(12), 99)),
   // animofs, scriptofs
-  Buffer.from(new Uint32Array([2, 1]).buffer),
+  B.u32([2, 1]),
   // face, actm, movecode, movescript, ctr, mode, modePadding (2 bytes)
-  Buffer.from(fill(Array(6 + 2), 88)),
+  B.u8(fill(Array(6 + 2), 88)),
   // step, delay, stepctr, delayctr, data1..data6
-  Buffer.from(new Uint16Array(fill(Array(10), 77)).buffer),
+  B.u16(fill(Array(10), 77)),
   // actscript, expand1..expand4
-  Buffer.from(new Uint32Array(fill(Array(5), 66)).buffer),
+  B.u32(fill(Array(5), 66)),
   // desc
-  Buffer.from(padEnd('Description', 20, '\0')),
+  B.stringFixed(20, 'Description'),
 ])
 
 const numEntities = 3
-const mapEntities = Buffer.concat([
+const mapEntities = makeBuffer([
   Buffer.from([numEntities]),
-  Buffer.concat(fill(Array(numEntities), entity))
+  makeBuffer(fill(Array(numEntities), entity))
 ])
 
-const scripts = Buffer.concat([
+const scripts = makeBuffer([
   // nummovescripts
-  Buffer.from([5]),
+  B.u8(5),
   // msbufsize
-  Buffer.from(new Uint32Array([50]).buffer),
+  B.u32(50),
   // msofstbl
-  Buffer.from(new Uint32Array([0, 10, 20, 30, 40]).buffer),
+  B.u32([0, 10, 20, 30, 40]),
   // msbuf
-  Buffer.from(fill(Array(50), 99)),
+  B.u8(fill(Array(50), 99)),
 
   // numthings
-  Buffer.from(new Uint32Array([0]).buffer),
+  B.u32(0),
 
   // mapevents
-  Buffer.from(new Uint32Array([2]).buffer),
+  B.u32(2),
   // mapvctbl
-  Buffer.from(new Uint32Array([0, 10]).buffer),
+  B.u32([0, 10]),
   // codesize
-  Buffer.from(new Uint32Array([20]).buffer),
+  B.u32(20),
   // mapvc
-  Buffer.from(fill(Array(20), 88)),
+  B.u8(fill(Array(20), 88)),
 ])
 
-const map = Buffer.concat([
+const map = makeBuffer([
   mapHeader,
   mapLayers,
   mapZones,
@@ -147,9 +144,6 @@ module.exports = {
   mapWidth,
   mapHeight,
   mapHeader,
-  // mapBackgroundLayerData,
-  // mapForegroundLayerData,
-  // mapObstructionLayerData,
   mapLayers,
   zone,
   mapZones,
