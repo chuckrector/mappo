44da1661999081ef80d578e986c9f9893b5d6931
Charles Rector <choosetheforce@gmail.com>
2016-09-12 00:42:37 -0700

    fix bug in v1chr2png cli where <30 frames would explode

--- src/formats/v1chr.js
+++ src/formats/v1chr.js
@@ -3,5 +3,8 @@
 const {T} = require('../readFormat')
 
 module.exports = {
-  chrs: T.list(T.u8, 16 * 32 * 30)
+  // v1 chrs are 16x32 and can hold up to 30 frames. some contain
+  // fewer frames, such as DARIN2.CHR, DARIN3.CHR, etc. (20 frames)
+  // just grab the whole file, no need to be picky
+  chrs: T.list(T.u8, ({reader}) => reader.remaining)
 }
