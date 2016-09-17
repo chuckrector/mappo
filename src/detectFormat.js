ead80848214d596d147b185a3b63ff23666c92b9
Charles Rector <choosetheforce@gmail.com>
2016-09-17 10:18:20 -0700

    formatting

--- src/detectFormat.js
+++ src/detectFormat.js
@@ -1,11 +1,11 @@
 "use strict"
 
 module.exports = (buffer) => {
-    if (buffer.length === 320 * 66) {
-        return 'v1boxraw'
-    } else if (buffer.length === 16 * 32 * 20 || buffer.length === 16 * 32 * 30) {
-        return 'v1chr'
-    }
+  if (buffer.length === 320 * 66) {
+    return 'v1boxraw'
+  } else if (buffer.length === 16 * 32 * 20 || buffer.length === 16 * 32 * 30) {
+    return 'v1chr'
+  }
 
-    return 'unknown'
+  return 'unknown'
 }
