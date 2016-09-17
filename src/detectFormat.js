0870c2ba64f024ea8573ce4e11213c3e9cc58300
Charles Rector <choosetheforce@gmail.com>
2016-09-17 10:28:45 -0700

    simplify

--- src/detectFormat.js
+++ src/detectFormat.js
@@ -1,17 +1,19 @@
 "use strict"
 
 module.exports = (buffer) => {
-  if (buffer.length === 320 * 66) {
+  switch (buffer.length) {
+  case 320 * 66:
     return 'v1boxraw'
-  } else if (buffer.length === 16 * 32 * 20 || buffer.length === 16 * 32 * 30) {
+  case 16 * 32 * 20:
+  case 16 * 32 * 30:
     return 'v1chr'
-  } else if (buffer.length === 3 * 256) {
+  case 3 * 256:
     return 'v1pal'
-  } else if (buffer.length === 96 * 96) {
+  case 96 * 96:
     return 'v1cr2'
-  } else if (buffer.length === 9 * 16 * 95) {
+  case 9 * 16 * 95:
     return 'v1mainfnt'
-  } else if (buffer.length === 7 * 9 * 95) {
+  case 7 * 9 * 95:
     return 'v1smallfnt'
   }
 
