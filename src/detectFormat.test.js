64890374cd49ffb164eb4b29b6216ff92079e677
Charles Rector <choosetheforce@gmail.com>
2016-09-17 10:18:42 -0700

    formatting

--- src/detectFormat.test.js
+++ src/detectFormat.test.js
@@ -4,21 +4,21 @@ const expect = require('expect')
 const detectFormat = require('./detectFormat')
 
 {
-    // can detect v1 BOX.RAW
-    const isBoxRaw = {length: 320 * 66}
-    const isNotBoxRaw = {length: 1}
+  // can detect v1 BOX.RAW
+  const isBoxRaw = {length: 320 * 66}
+  const isNotBoxRaw = {length: 1}
 
-    expect(detectFormat(isBoxRaw)).toBe('v1boxraw')
-    expect(detectFormat(isNotBoxRaw)).toBe('unknown')
+  expect(detectFormat(isBoxRaw)).toBe('v1boxraw')
+  expect(detectFormat(isNotBoxRaw)).toBe('unknown')
 }
 
 {
-    // can detect v1 CHR
-    const isV1Chr = {length: 16 * 32 * 30}
-    const isAlsoV1Chr = {length: 16 * 32 * 20}
-    const isNotV1Chr = {length: 1}
+  // can detect v1 CHR
+  const isV1Chr = {length: 16 * 32 * 30}
+  const isAlsoV1Chr = {length: 16 * 32 * 20}
+  const isNotV1Chr = {length: 1}
 
-    expect(detectFormat(isV1Chr)).toBe('v1chr')
-    expect(detectFormat(isAlsoV1Chr)).toBe('v1chr')
-    expect(detectFormat(isNotV1Chr)).toBe('unknown')
+  expect(detectFormat(isV1Chr)).toBe('v1chr')
+  expect(detectFormat(isAlsoV1Chr)).toBe('v1chr')
+  expect(detectFormat(isNotV1Chr)).toBe('unknown')
 }
