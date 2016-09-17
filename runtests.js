036f890c2eac389d6ceac07e4b2ffee5c92aed4a
Charles Rector <choosetheforce@gmail.com>
2016-09-17 10:16:54 -0700

    ability to filter tests. add convenience wrapper

--- runtests.js
+++ runtests.js
@@ -1,8 +1,33 @@
 var glob = require('glob')
 
+console.log('running tests...')
+
+let filter = false
+if (process.argv.length === 3) {
+  filter = process.argv[2]
+  console.log('filtering filenames by:', filter)
+}
+
 glob('src/**/*.test.js', (err, files) => {
+  let testsRan = 0
+
   files.forEach((testJs) => {
-    console.log('running tests:', testJs)
-    require('./' + testJs)
+    let runTest = true
+    if (filter && !testJs.toLowerCase().includes(filter.toLowerCase())) {
+      runTest = false
+    }
+
+    if (runTest) {
+      console.log('running tests:', testJs)
+      require('./' + testJs)
+      testsRan++
+    }
   })
+
+  if (testsRan < 1) {
+    console.log('*error* no matching tests found')
+    process.exit(1)
+  } else {
+    console.log('*OK* all tests passed')
+  }
 })
