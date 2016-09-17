df5fce586acfd4a8801fe78ba990135faa330053
Charles Rector <choosetheforce@gmail.com>
2016-09-17 11:45:40 -0700

    sort formats by size

--- src/detectFormat.js
+++ src/detectFormat.js
@@ -2,24 +2,33 @@
 
 module.exports = (buffer) => {
   switch (buffer.length) {
-  case 320 * 66:
-    return 'v1boxraw'
-  // could be a CHR with 10 or 15 frames; CHR usually <=5
-  case 16 * 32 * 20:
-  case 16 * 32 * 30:
-    return 'v1chr'
-  case 3 * 256:
-    return 'v1pal'
-  // could be a CHR with 9 frames; CHR usually <=5
-  case 96 * 96:
-    return 'v1cr2'
-  case 9 * 16 * 95:
-    return 'v1mainfnt'
-  case 7 * 9 * 95:
-    return 'v1smallfnt'
   // could be a CHR with 64 frames; CHR usually <=5
+  // 65536
   case 256 * 256:
     return 'v1transtbl'
+  // 21120
+  case 320 * 66:
+    return 'v1boxraw'
+  // 15360
+  case 16 * 32 * 30:
+    return 'v1chr'
+  // 13680
+  case 9 * 16 * 95:
+    return 'v1mainfnt'
+  // could be a CHR with 10 or 15 frames; CHR usually <=5
+  // 10240
+  case 16 * 32 * 20:
+    return 'v1chr'
+  // could be a CHR with 9 frames; CHR usually <=5
+  // 9216
+  case 96 * 96:
+    return 'v1cr2'
+  // 5985
+  case 7 * 9 * 95:
+    return 'v1smallfnt'
+  // 768
+  case 3 * 256:
+    return 'v1pal'
   }
 
   if (buffer.length % (32 * 32) === 0) {
