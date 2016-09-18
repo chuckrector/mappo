d16173f331ad6984076645fbee4476547e6efa8f
Charles Rector <choosetheforce@gmail.com>
2016-09-17 20:11:59 -0700

    declarative v2kj chr loader

---
+++ src/formats/v2kjchr.js
@@ -0,0 +1,27 @@
+"use strict"
+
+const {T} = require('../readFormat')
+
+module.exports = {
+  version: T.u8,
+  fxsize: T.u16,
+  fysize: T.u16,
+  hx: T.u16,
+  hy: T.u16,
+  hw: T.u16,
+  hh: T.u16,
+  lidle: T.u32,
+  ridle: T.u32,
+  uidle: T.u32,
+  didle: T.u32,
+  totalframes: T.u16,
+  lanimLength: T.u32,
+  lanim: T.stringFixed(({record}) => record.lanimLength),
+  ranimLength: T.u32,
+  ranim: T.stringFixed(({record}) => record.ranimLength),
+  uanimLength: T.u32,
+  uanim: T.stringFixed(({record}) => record.uanimLength),
+  danimLength: T.u32,
+  danim: T.stringFixed(({record}) => record.danimLength),
+  imagedata: T.compressedU6(({record}) => record.fxsize * record.fysize * record.totalframes),
+}
