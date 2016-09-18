df03c4a91a9bb3a3655ceb88a1421f29b0ede136
Chuck <choosetheforce@gmail.com>
2016-09-17 21:29:17 -0700

    verge2kj chr converter

---
+++ src/converter/createVerge2kjChrConverter.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const createTileGridConverter = require('./createTileGridConverter')
+const colorDepth = require('./colorDepth')
+
+module.exports = ({fxsize, fysize, totalframes, imagedata}) => {
+  const converter = createTileGridConverter({
+    tileWidth: fxsize,
+    tileHeight: fysize,
+    columns: 5,
+    numtiles: totalframes,
+    raw32bitData: colorDepth.convert16to32({raw16bitData: imagedata}),
+  })
+
+  return {
+    convertToPng: converter.convertToPng
+  }
+}
---
+++ src/converter/createVerge2kjChrConverter.test.js
@@ -0,0 +1,25 @@
+"use strict"
+
+const expect = require('expect')
+const createVerge2kjChrConverter = require('./createVerge2kjChrConverter')
+const filler = require('../filler')
+
+{
+  // can convert chr image data to png
+  const fxsize = 2
+  const fysize = 3
+  const totalframes = 6
+  const imagedata = filler(fxsize * fysize * totalframes, 99)
+
+  const converter = createVerge2kjChrConverter({
+    fxsize,
+    fysize,
+    totalframes,
+    imagedata,
+  })
+
+  const png = converter.convertToPng()
+
+  expect(png.width).toBe(fxsize * 5)
+  expect(png.height).toBe(fysize * 2)
+}
