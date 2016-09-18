a11fe46c030f7058af91a74d363790254418cbfc
Chuck <choosetheforce@gmail.com>
2016-09-17 21:56:49 -0700

    v2kjchr2pnglist cli

---
+++ src/cli/v2kjchr2pnglist.js
@@ -0,0 +1,38 @@
+const process = require('process')
+
+const fs = require('fs')
+const colorDepth = require('../converter/colorDepth')
+const ripTiles = require('../ripTiles')
+const {PNG} = require('pngJS')
+const asset = require('../asset')
+
+const chrFilename = process.argv[2]
+const chrData = asset.fromDisk(chrFilename, asset.v2kjchr)
+const raw32BitData = colorDepth.convert16to32({
+  raw16bitData: chrData.imagedata.decompressed,
+})
+
+const tileWidth = chrData.fxsize
+const tileHeight = chrData.fysize
+const tileList = ripTiles({
+  raw32BitData,
+  raw32BitDataWidth: tileWidth,
+  raw32BitDataHeight: tileHeight * chrData.totalframes,
+  ripFromX: 0,
+  ripFromY: 0,
+  tileWidth: tileWidth,
+  tileHeight: tileHeight,
+  numTiles: chrData.totalframes,
+  numColumns: 1,
+})
+
+console.log('converting', chrFilename)
+tileList.forEach((tile, tileIndex) => {
+  const targetFilename = chrFilename + '-' + tileIndex + '.png'
+  const png = new PNG({width: tileWidth, height: tileHeight})
+  for (let p = 0; p < tileWidth * tileHeight * 4; p++) {
+    png.data[p] = tile[p]
+  }
+  png.pack().pipe(fs.createWriteStream(targetFilename))
+  console.log('generated', targetFilename)
+})
