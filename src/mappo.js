fee6b72540db4132b1945cbdf7fae013782ebd57
Chuck <choosetheforce@gmail.com>
2016-09-18 23:58:56 -0700

    learned how to render a tile

--- src/mappo.js
+++ src/mappo.js
@@ -4,6 +4,7 @@ const $ = require('jquery')
 const jQuery = $
 require('./jquery-ui-1.12.0.custom.le-frog/jquery-ui.min.js')
 const asset = require('./asset')
+const colorDepth = require('./converter/colorDepth')
 
 $('body').resizable()
 
@@ -19,6 +20,24 @@ console.log('numscripts', mapData.numscripts)
 console.log('scriptofstbl', JSON.stringify(mapData.scriptofstbl))
 console.log('mapvcs', mapData.mapvc.length)
 
+const palFilename = 'data/v1/VERGE.PAL'
+const palData = asset.fromDisk(palFilename, asset.v1pal)
+
 const vspFilename = 'data/v1/' + mapData.vsp0name
 const vspData = asset.fromDisk(vspFilename, asset.v1vsp)
 console.log('vsp', vspFilename, vspData)
+
+const raw32bitData = colorDepth.convert8to32({
+  palette: palData.pal.map(v => v * 4),
+  raw8bitData: vspData.vsp0,
+})
+
+const $canvas = $('.mappo-viewport')
+const context = $canvas[0].getContext('2d')
+
+const image = context.createImageData(16, 16)
+for (let i = 0; i < 16 * 16 * 4; i++) {
+  image.data[i] = raw32bitData[i]
+}
+
+context.putImageData(image, 0, 0)
