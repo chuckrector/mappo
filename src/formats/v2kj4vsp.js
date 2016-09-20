5ed274e5b48ea53bb1a209e48ae8496e942c26e3
Chuck <choosetheforce@gmail.com>
2016-09-19 18:41:50 -0700

    declarative v2kj v4 vsp loader (uncompressed hicolor)

---
+++ src/formats/v2kj4vsp.js
@@ -0,0 +1,12 @@
+"use strict"
+
+const {T} = require('../readFormat')
+const V1_VSPANIM = require('./v1vspanim')
+
+module.exports = {
+  version: T.u16,
+  palette: T.list(T.u8, 3 * 256),
+  numtiles: T.u16,
+  imagedata: T.u16(({record}) => 16 * 16 * record.numtiles),
+  vspanim: T.list(V1_VSPANIM, 100),
+}
