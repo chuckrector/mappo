5b4b0c172e4d73cccdbe7557c674067db6718384
Chuck <choosetheforce@gmail.com>
2016-09-26 10:52:51 -0700

    strings to template strings

--- runtests.js
+++ runtests.js
@@ -1,14 +1,14 @@
-var glob = require('glob')
+var glob = require(`glob`)
 
-console.log('running tests...')
+console.log(`running tests...`)
 
 let filter = false
 if (process.argv.length === 3) {
   filter = process.argv[2]
-  console.log('filtering filenames by:', filter)
+  console.log(`filtering filenames by:`, filter)
 }
 
-glob('src/**/*.test.js', (err, files) => {
+glob(`src/**/*.test.js`, (err, files) => {
   let testsRan = 0
 
   files.forEach((testJs) => {
@@ -18,16 +18,16 @@ glob('src/**/*.test.js', (err, files) => {
     }
 
     if (runTest) {
-      console.log('running tests:', testJs)
-      require('./' + testJs)
+      console.log(`running tests:`, testJs)
+      require(`./` + testJs)
       testsRan++
     }
   })
 
   if (testsRan < 1) {
-    console.log('*error* no matching tests found')
+    console.log(`*error* no matching tests found`)
     process.exit(1)
   } else {
-    console.log('*OK* all tests passed')
+    console.log(`*OK* all tests passed`)
   }
 })
