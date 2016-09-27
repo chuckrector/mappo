cf334cc5242a5341eb29fbe58fafd28c90fcdb2e
Chuck <choosetheforce@gmail.com>
2016-09-26 23:08:44 -0700

    redux: add UNDO action

--- src/mappoState.js
+++ src/mappoState.js
@@ -2,6 +2,9 @@
 
 const cloneDeep = require(`lodash/clonedeep`)
 
+const undo = []
+let undoCursor = 0
+
 module.exports = (state={}, action) => {
   switch (action.type) {
     case `SET_MAP`: {
@@ -16,6 +19,7 @@ module.exports = (state={}, action) => {
       const newState = cloneDeep(state)
       const layer = newState.map.tileLayers[action.layerIndex]
       const layerOffset = (action.y * layer.width) + action.x
+      const overwrittenTileIndex = layer.tileIndexGrid[layerOffset]
 
       layer.tileIndexGrid = [
         ...layer.tileIndexGrid.slice(0, layerOffset),
@@ -23,8 +27,29 @@ module.exports = (state={}, action) => {
         ...layer.tileIndexGrid.slice(layerOffset + 1),
       ]
 
+      undo.push({
+        originalAction: cloneDeep(action),
+        overwrittenTileIndex,
+      })
+      undoCursor++
+
       return newState
     }
+
+    case `UNDO`: {
+      const undoInfo = undo[--undoCursor]
+      const newState = cloneDeep(state)
+      const layer = newState.map.tileLayers[undoInfo.originalAction.layerIndex]
+      const layerOffset = (undoInfo.originalAction.y * layer.width) + undoInfo.originalAction.x
+
+      layer.tileIndexGrid = [
+        ...layer.tileIndexGrid.slice(0, layerOffset),
+        undoInfo.overwrittenTileIndex,
+        ...layer.tileIndexGrid.slice(layerOffset + 1),
+      ]
+
+      return newState
+    } break
 
     default: {
       return state
--- src/mappoState.test.js
+++ src/mappoState.test.js
@@ -26,3 +26,15 @@ const mappoState = require(`./mappoState`)
   store.dispatch({type: `PLOT_TILE`, x: 0, y: 1, layerIndex: 0, tileIndex: 99})
   expect(store.getState().map.tileLayers[0].tileIndexGrid).toEqual([0, 0, 99, 0])
 }
+
+{
+  // can undo a plotted tile
+  const store = createStore(mappoState)
+
+  store.dispatch({type: `SET_MAP`, tileLayers: [{width: 2, height: 2, tileIndexGrid: filler(2 * 2, 0)}]})
+  expect(store.getState().map.tileLayers[0].tileIndexGrid).toEqual(filler(2 * 2, 0))
+
+  store.dispatch({type: `PLOT_TILE`, x: 0, y: 1, layerIndex: 0, tileIndex: 99})
+  store.dispatch({type: `UNDO`})
+  expect(store.getState().map.tileLayers[0].tileIndexGrid).toEqual(filler(2 * 2, 0))
+}
