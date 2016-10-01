b26457c8e2cd74144f28aa83284895fd8ab70c54
Chuck <choosetheforce@gmail.com>
2016-10-01 09:00:34 -0700

    there is no v2-specific palette format. it's all v1

--- runtests-cli.sh
+++ runtests-cli.sh
@@ -21,10 +21,10 @@ set -e
 "node src/cli/v1speechspc2png.js data/v1/VERGE.PAL data/v1/SPEECH.SPC" \
 "node src/cli/v1transtbl2json.js data/v1/TRANS.TBL" \
 "node src/cli/v1vsp2png.js data/v1/HAHN01.VSP" \
-"node src/cli/v2chr2gif.js data/v2/VERGE2.PAL data/v2/VECNA.CHR" \
-"node src/cli/v2chr2json.js data/v2/VERGE2.PAL data/v2/VECNA.CHR" \
-"node src/cli/v2chr2png.js data/v2/VERGE2.PAL data/v2/VECNA.CHR" \
-"node src/cli/v2chr2pnglist.js data/v2/VERGE2.PAL data/v2/VECNA.CHR" \
+"node src/cli/v2chr2gif.js data/v1/VERGE.PAL data/v2/VECNA.CHR" \
+"node src/cli/v2chr2json.js data/v1/VERGE.PAL data/v2/VECNA.CHR" \
+"node src/cli/v2chr2png.js data/v1/VERGE.PAL data/v2/VECNA.CHR" \
+"node src/cli/v2chr2pnglist.js data/v1/VERGE.PAL data/v2/VECNA.CHR" \
 "node src/cli/v2map2json.js data/v2/v2world.map" \
 "node src/cli/v2vsp2pal.js data/v2/GRUE0041.VSP" \
 "node src/cli/v2vsp2png.js data/v2/GRUE0041.VSP" \
