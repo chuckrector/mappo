3c30229badf55171e1d748c7b781e1dd532e1171
Chuck <choosetheforce@gmail.com>
2016-10-02 20:29:54 -0700

    declarative verge2.7 (ika) 8-bit vsp loader

---
+++ src/formats/v27vsp8bit.js
@@ -0,0 +1,19 @@
+"use strict"
+
+const {T} = require(`../readFormat`)
+const V1_VSPANIM = require(`./v1vspanim`)
+
+module.exports = {
+  version: T.u16,
+  bytesPerPixel: T.u8,
+  tileWidth: T.u16,
+  tileHeight: T.u16,
+  numTiles: T.u32,
+  description: T.stringFixed(64),
+  palette: T.list(T.u8, 3 * 256),
+  transparentIndex: T.u8,
+  imageData: T.ikaZlibU8(({record}) => (
+    record.tileWidth * record.tileHeight * record.numTiles
+  )),
+  animations: T.list(V1_VSPANIM, 100),
+}
