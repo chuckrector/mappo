d178feff993c4a232ff7969efd49659e7af6c4e1
Chuck <choosetheforce@gmail.com>
2016-10-04 20:43:50 -0700

    remember editor window position

--- src/index.js
+++ src/index.js
@@ -3,45 +3,84 @@
 // This is the Electron entry point. The chain of command is:
 // Electron binary -> index.js -> index.html -> mappo.js
 
+const electron = require(`electron`)
 const {app, BrowserWindow} = require(`electron`)
 const fs = require(`fs`)
 
 let win
 
-const createWindow = () => {
-  let mappoConfigText
-  let mappoConfig = {}
-  let windowSize = {
-    width: 1440,
-    height: 900,
-  }
-
+const loadMappoConfig = () => {
   try {
-    mappoConfigText = fs.readFileSync(`mappo.json`)
+    const mappoConfigText = fs.readFileSync(`mappo.json`)
 
     try {
-      mappoConfig = JSON.parse(mappoConfigText)
+      return JSON.parse(mappoConfigText)
     } catch (exc) {
       console.log(`bad config file, using defaults. [error: ${exc.message}]`)
-    }
-
-    if (mappoConfig.width && mappoConfig.height) {
-      console.log(`setting editor window size to`, mappoConfig.windowSize)
-      windowSize = {
-        width: mappoConfig.width,
-        height: mappoConfig.height,
-      }
     }
   } catch (exc) {
     console.log(`no mappo.json found, using defaults. [error: ${exc.message}]`)
   }
 
-  win = new BrowserWindow(windowSize)
+  return {}
+}
+
+const saveMappoConfig = config => {
+  let mappoConfigText
+  try {
+    mappoConfigText = JSON.stringify(config)
+  } catch (exc) {
+    console.log(`bad config object, not saving config. [error: ${exc.message}]`)
+    return
+  }
+
+  try {
+    fs.writeFileSync(`mappo.json`, mappoConfigText)
+    console.log(`editor state successfully saved`)
+  } catch (exc) {
+    console.log(`unable to write config to disk. [error: ${exc.message}]`)
+  }
+}
+
+const createWindow = () => {
+  let windowBounds = {
+    width: 1440,
+    height: 900,
+  }
+
+  let mappoConfig = loadMappoConfig()
+  if (mappoConfig.width && mappoConfig.height) {
+    console.log(`setting editor window size: ${mappoConfig.width}x${mappoConfig.height}`)
+    windowBounds.width = mappoConfig.width
+    windowBounds.height = mappoConfig.height
+  }
+
+  if (mappoConfig.x && mappoConfig.y) {
+    console.log(`setting editor window position: ${mappoConfig.x},${mappoConfig.y}`)
+    windowBounds.x = mappoConfig.x
+    windowBounds.y = mappoConfig.y
+  }
+
+  win = new BrowserWindow(windowBounds)
 
   win.loadURL(`file://${__dirname}/index.html`)
 
   win.webContents.on(`devtools-opened`, win.webContents.focus)
   win.webContents.openDevTools()
+
+  const saveEditorBounds = () => {
+    const config = loadMappoConfig()
+    const bounds = win.getBounds()
+    console.log(`saving editor bounds: ${JSON.stringify(bounds)}`)
+    config.x = bounds.x
+    config.y = bounds.y
+    config.width = bounds.width
+    config.height = bounds.height
+    saveMappoConfig(config)
+  }
+
+  win.on(`moved`, saveEditorBounds)
+  win.on(`resize`, saveEditorBounds)
 
   win.on(`closed`, () => {
     win = null
