d1e0b15085b6d7651e9c546d8f6d3372176ce53c
Chuck <choosetheforce@gmail.com>
2016-10-05 23:12:23 -0700

    redux: add plots reducer. prep for moving high-level undoable reducer off of map reducer

---
+++ src/plots.js
@@ -0,0 +1,37 @@
+"use strict"
+
+const immutableArraySet = require(`./immutableArraySet`)
+
+module.exports = (state=[], action) => {
+  switch (action.type) {
+    case `PLOT_TILE`: {
+      const {
+        x,
+        y,
+        tileIndexToPlot,
+        tileIndexGridWidth,
+        layerIndex,
+        tileLayers,
+      } = action
+      const layer = tileLayers[layerIndex]
+      const tileIndexGridOffset = (y * tileIndexGridWidth) + x
+      const overwritingTileIndex = layer.tileIndexGrid[tileIndexGridOffset]
+      return immutableArraySet({
+        array: state,
+        index: state.length,
+        newValue: {
+          x: action.x,
+          y: action.y,
+          tileIndexToPlot: action.tileIndexToPlot,
+          layerIndex: action.layerIndex,
+          tileIndexGridWidth: action.tileIndexGridWidth,
+          overwritingTileIndex,
+        },
+      })
+    } break
+
+    default: {
+      return state
+    }
+  }
+}
---
+++ src/plots.test.js
@@ -0,0 +1,35 @@
+"use strict"
+
+const expect = require(`expect`)
+const createStore = require(`./createStore`)
+const plots = require(`./plots`)
+
+{
+  // can add plot
+  const store = createStore(plots)
+  expect(store.getState()).toEqual([])
+
+  const tileLayers = [
+    {tileIndexGrid: [0, 0, 0, 0]},
+    {tileIndexGrid: [1, 1, 1, 1]},
+  ]
+
+  store.dispatch({
+    type: `PLOT_TILE`,
+    x: 0,
+    y: 1,
+    tileIndexToPlot: 99,
+    tileIndexGridWidth: 2,
+    layerIndex: 1,
+    tileLayers,
+  })
+
+  expect(store.getState()).toEqual([{
+    x: 0,
+    y: 1,
+    tileIndexToPlot: 99,
+    tileIndexGridWidth: 2,
+    layerIndex: 1,
+    overwritingTileIndex: 1,
+  }])
+}
